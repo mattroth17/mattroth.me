ba25eccbe11787aa0002b492fa3527c7a9eb3b3e
matt roth <matthew.c.roth.22@dartmouth.edu>
2020-07-13 13:06:51 -0500

    final commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+$('#main').html('Here we go!');
+
+let seconds = 0;
+
+setInterval(() => { seconds += 1; $('#main').html(`You've been on this page for ${seconds} seconds.`); }, 1000);
